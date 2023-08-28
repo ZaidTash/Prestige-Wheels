9cdd4293c2bdaec890e558b694da5e27033d5097
Mohmad Bino <Mohmad Bino@DESKTOP-CK6IHR4>
2023-08-28 18:03:11 +0300

    Add project files.

---
+++ Prestige-Wheels/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prestige_Wheels.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
